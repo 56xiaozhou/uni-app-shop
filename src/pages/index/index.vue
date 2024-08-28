36fece42bf4c0ea978c6a414d602525e6e3efcf5
56xiaozhou <492020291@qq.com>
2024-08-28 13:40:44 +0800

    feat：轮播图类型定义和渲染

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,11 +1,25 @@
 <script setup lang="ts">
 import CustomNavbar from '@/pages/index/components/CustomNavbar.vue'
 import MySwiper from '@/components/MySwiper.vue'
+import { getHomeBannerAPI } from '@/services/home'
+import { onLoad } from '@dcloudio/uni-app'
+import { ref } from 'vue'
+import type { BannerItem } from '@/types/home'
+
+const bannerList = ref<BannerItem[]>([])
+const getHomeBannerData = async () => {
+  const res = await getHomeBannerAPI()
+  bannerList.value = res.result
+}
+
+onLoad(() => {
+  getHomeBannerData()
+})
 </script>
 
 <template>
   <CustomNavbar />
-  <MySwiper />
+  <MySwiper :list="bannerList" />
   <view class="index">index</view>
 </template>
 
---
+++ src/services/home.ts
@@ -0,0 +1,16 @@
+import { http } from '@/utils/http'
+import type { BannerItem } from '@/types/home'
+
+/**
+ * 首页-广告区域-小程序
+ * @param distributionSite 广告区域展示位置（1：首页；2：分类商品页）默认1
+ */
+export const getHomeBannerAPI = (distributionSite = 1) => {
+  return http<BannerItem[]>({
+    method: 'GET',
+    url: '/home/banner',
+    data: {
+      distributionSite,
+    },
+  })
+}
