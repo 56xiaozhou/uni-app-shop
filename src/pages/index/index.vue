7621e7b306e3fa85c3a9b00371cf2f2be8c22be0
56xiaozhou <492020291@qq.com>
2024-08-28 14:11:06 +0800

    feat：前台分类组件封装

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import CustomNavbar from '@/pages/index/components/CustomNavbar.vue'
 import MySwiper from '@/components/MySwiper.vue'
+import CategoryPanel from '@/pages/index/components/CategoryPanel.vue'
 import { getHomeBannerAPI } from '@/services/home'
 import { onLoad } from '@dcloudio/uni-app'
 import { ref } from 'vue'
@@ -18,11 +19,17 @@ onLoad(() => {
 </script>
 
 <template>
+  <!-- 自定义导航栏 -->
   <CustomNavbar />
+  <!-- 自定义轮播图 -->
   <MySwiper :list="bannerList" />
+  <!-- 分类面板 -->
+  <CategoryPanel />
   <view class="index">index</view>
 </template>
 
 <style lang="scss">
-//
+page {
+  background-color: #f7f7f7;
+}
 </style>
