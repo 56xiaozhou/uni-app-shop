b8a8a71884db8c28b9d8da5e39217171e032d8c1
56xiaozhou <492020291@qq.com>
2024-08-30 19:58:46 +0800

    feat：热门推荐分页条件

--- src/pages/hot/hot.vue
+++ src/pages/hot/hot.vue
@@ -25,12 +25,16 @@ uni.setNavigationBarTitle({ title: currHotMap!.title })
 // 推荐封面图
 const bannerPicture = ref('')
 // 推荐选项
-const subTypes = ref<SubTypeItem[]>([])
+const subTypes = ref<(SubTypeItem & { finish?: boolean })[]>([])
 // 高亮的下表
 const activeIndex = ref(0)
 // 获取热门推荐数据
 const getHotRecommendData = async () => {
-  const res = await getHotRecommendAPI(currHotMap!.url)
+  const res = await getHotRecommendAPI(currHotMap!.url, {
+    // 技巧：环境变量，开发环境：修改初始页码方便测试分页结束
+    page: import.meta.env.DEV ? 30 : 1,
+    pageSize: 10,
+  })
   bannerPicture.value = res.result.bannerPicture
   subTypes.value = res.result.subTypes
 }
@@ -38,19 +42,30 @@ const getHotRecommendData = async () => {
 // 滚动触底
 const onScrollToLower = async () => {
   // 获取当前的选项
-  const curSubType = subTypes.value[activeIndex.value]
-  // 当前页码累加
-  curSubType.goodsItems.page++
+  const curSubTypes = subTypes.value[activeIndex.value]
+  // 分页条件
+  if (curSubTypes.goodsItems.page < curSubTypes.goodsItems.pages) {
+    // 当前页码累加
+    curSubTypes.goodsItems.page++
+  } else {
+    // 标记已结束
+    curSubTypes.finish = true
+    // 退出并轻提示
+    return uni.showToast({
+      icon: 'none',
+      title: '没有更多数据了~',
+    })
+  }
   // 调用API传参
   const res = await getHotRecommendAPI(currHotMap!.url, {
-    subType: curSubType.id,
-    page: curSubType.goodsItems.page,
-    pageSize: curSubType.goodsItems.pageSize,
+    subType: curSubTypes.id,
+    page: curSubTypes.goodsItems.page,
+    pageSize: curSubTypes.goodsItems.pageSize,
   })
   // 新的列表选项
   const newSubTypes = res.result.subTypes[activeIndex.value]
   // 数组追加
-  curSubType.goodsItems.items.push(...newSubTypes.goodsItems.items)
+  curSubTypes.goodsItems.items.push(...newSubTypes.goodsItems.items)
 }
 
 // 页面加载
@@ -101,7 +116,9 @@ onLoad(() => {
           </view>
         </navigator>
       </view>
-      <view class="loading-text">正在加载...</view>
+      <view class="loading-text">
+        {{ item.finish ? '没有更多数据了~' : '正在加载...' }}
+      </view>
     </scroll-view>
   </view>
 </template>
