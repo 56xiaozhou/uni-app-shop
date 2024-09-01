0b9def9196daec26317610d4f8716757f3547c14
56xiaozhou <492020291@qq.com>
2024-09-01 12:21:45 +0800

    feat：商品详情-轮播图交互

--- src/pages/goods/goods.vue
+++ src/pages/goods/goods.vue
@@ -20,6 +20,21 @@ const getGoodsByIdData = async () => {
   goods.value = res.result
 }
 
+// 轮播图变化时
+const currIndex = ref(0)
+const onChange: UniHelper.Swiper.SwiperOnChange = (ev) => {
+  currIndex.value = ev.detail!.current
+}
+
+// 点击图片时
+const onTapImage = (url: string) => {
+  // 大图预览
+  uni.previewImage({
+    current: url,
+    urls: goods.value!.mainPictures,
+  })
+}
+
 // 页面加载
 onLoad(() => {
   getGoodsByIdData()
@@ -32,15 +47,15 @@ onLoad(() => {
     <view class="goods">
       <!-- 商品主图 -->
       <view class="preview">
-        <swiper circular>
-          <swiper-item v-for="item in goods.mainPictures" :key="item">
-            <image mode="aspectFill" :src="item" />
+        <swiper circular @change="onChange">
+          <swiper-item v-for="item in goods?.mainPictures" :key="item">
+            <image mode="aspectFill" :src="item" @tap="onTapImage(item)" />
           </swiper-item>
         </swiper>
         <view class="indicator">
-          <text class="current">1</text>
+          <text class="current">{{ currIndex + 1 }}</text>
           <text class="split">/</text>
-          <text class="total">5</text>
+          <text class="total">{{ goods?.mainPictures.length }}</text>
         </view>
       </view>
 
